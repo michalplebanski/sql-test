ec5f6b26ee482b4ba99addef2df970de690577b6
Michał Plebański <michalplebanski@gmail.com>
2023-06-25 15:17:07 +0200

    execute SQL to add tables

--- main.py
+++ main.py
@@ -9,9 +9,8 @@ def create_connection(db_file):
         print(f"Connected to {db_file}, sqlite version: {sqlite3.version} ")
     except Error as e:
         print(e)
-    finally:
-        if conn:
-            conn.close()
+    
+    return conn
 
 def create_connection_in_memory():
     """ create a database connection to a SQLite database """
@@ -24,29 +23,45 @@ def create_connection_in_memory():
     finally:
         if conn:
             conn.close()
+        
+def execute_sql(conn, sql):
+    """ Execute sql
+    :param conn: Connection object
+    :param sql: a SQL script
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(sql)
+    except Error as e:
+        print(e)
 
 if __name__ == '__main__':
-    create_connection(r"database.db")
-    create_connection_in_memory()
+    create_urls_sql = """
+    -- URLS TABLE
+    CREATE TABLE IF NOT EXISTS urls (
+        id integer PRIMARY KEY,
+        nazwa text NOT NULL
+    );
+    """
 
+    create_meta_sql = """
+    --- META TABLE
+    CREATE TABLE IF NOT EXISTS meta (
+        id integer PRIMARY KEY,
+        projekt_id integer NOT NULL,
+        meta_title TEXT,
+        meta_description TEXT,
+        header_1 TEXT,
+        FOREIGN KEY (projekt_id) REFERENCES urls (id)
+    )
+    """
 
-create_urls_sql = """
--- URLS TABLE
-CREATE TABLE IF NOT EXISTS urls (
-    id integer PRIMARY KEY,
-    nazwa text NOT NULL
-);
-"""
-
-create_meta_sql = """
---- META TABLE
-CREATE TABLE IF NOT EXISTS meta (
-    id integer PRIMARY KEY,
-    projekt_id integer NOT NULL,
-    meta_title TEXT,
-    meta_description TEXT,
-    header_1 TEXT,
-    FOREIGN KEY (projekt_id) REFERENCES urls (id)
-)
-"""
+    db_file = "database.db"
+    conn = create_connection(db_file)
+    if conn is not None:
+        execute_sql(conn, create_urls_sql)
+        execute_sql(conn, create_meta_sql)
+        create_connection_in_memory()
+        conn.close()
 
