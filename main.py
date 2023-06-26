558860ba56af24458aca183d109f023f0b337d61
Michał Plebański <michalplebanski@gmail.com>
2023-06-26 21:57:01 +0200

    fix sql scripts

--- main.py
+++ main.py
@@ -7,22 +7,11 @@ def create_connection(db_file):
     try:
         conn = sqlite3.connect(db_file)
         print(f"Connected to {db_file}, sqlite version: {sqlite3.version} ")
+        return conn
     except Error as e:
         print(e)
     
     return conn
-
-def create_connection_in_memory():
-    """ create a database connection to a SQLite database """
-    conn = None
-    try:
-        conn = sqlite3.connect(":memory")
-        print(f"Connected, sqlite version: {sqlite3.version}")
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
         
 def execute_sql(conn, sql):
     """ Execute sql
@@ -35,6 +24,66 @@ def execute_sql(conn, sql):
         c.execute(sql)
     except Error as e:
         print(e)
+
+def add_urls(conn, urls):
+    """
+    Create a new record into the URLS table
+    :param conn:
+    :param project:
+    """
+    sql = '''INSERT INTO urls(nazwa) VALUES(?)'''
+    cur = conn.cursor()
+    cur.execute(sql, urls)
+    return cur.lastrowid
+
+def add_meta(conn, meta):
+    """
+    Create a new record into the META table
+    :param conn:
+    :param meta:
+    """
+    sql = '''INSERT INTO meta(id, projekt_id, meta_title, meta_description, header_1) VALUES(?,?,?,?,?)'''
+    cur = conn.cursor()
+    cur.execute(sql, meta)
+    return cur.lastrowid
+
+def select_meta_by_id(conn, id):
+    """
+    Query tasks by priority
+    :param conn: the Connection object
+    :param id
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM meta WHERE id=?", (str(id)))
+
+    rows = cur.fetchall()
+    return rows
+
+def update(conn, table, id, meta_title):
+    """
+    Update meta title
+    :param conn: the Connection object
+    :param table: table name
+    :id:
+    :meta_title
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute(f"UPDATE {table} SET meta_title = '{meta_title}' WHERE id = {id}")
+    conn.commit()
+
+def delete(conn, table, id):
+    """
+    Delete specific record
+    :param conn: the Connection object
+    :param table: table name
+    :id:
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute(f"DELETE FROM {table} WHERE id = {id}")
+    conn.commit()
 
 if __name__ == '__main__':
     create_urls_sql = """
@@ -59,9 +108,22 @@ if __name__ == '__main__':
 
     db_file = "database.db"
     conn = create_connection(db_file)
-    if conn is not None:
-        execute_sql(conn, create_urls_sql)
-        execute_sql(conn, create_meta_sql)
-        create_connection_in_memory()
-        conn.close()
-
+    execute_sql(conn, create_urls_sql)
+    execute_sql(conn, create_meta_sql)
+    urls = ("widoczni.pl",)
+    urls_2 = ("widzialni.pl",)
+    pr_id = add_urls(conn, urls)
+    meta = (
+        pr_id,
+        "Widoczni",
+        "Pozycjonowanie stron",
+        "Zapraszamy do najlepszej agancji",
+        "Pozycjonowanie"
+    )
+    add_meta(conn, meta)
+    print(select_meta_by_id(conn, 1))
+    update(conn, "meta", 1, "Widzialni")
+    add_urls(conn, urls_2)
+    delete(conn, "urls", id=2)
+    conn.commit()
+    conn.close()
